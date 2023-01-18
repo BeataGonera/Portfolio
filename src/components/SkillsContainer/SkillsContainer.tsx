6e3b5bc6aea31e3218e26db054f54651fe3ef36b
Beata Gonera <beataiwonagonera@gmail.com>
2023-01-18 10:02:53 +0100

    skills width changed

--- src/components/SkillsContainer/SkillsContainer.tsx
+++ src/components/SkillsContainer/SkillsContainer.tsx
@@ -4,7 +4,7 @@ import { SmallPresentationContainer } from "./SmallPresentationContainer";
 export const SkillsContainer = () => {
     return ( 
 
-        <div className=" bg-silver flex flex-col px-3 ">
+        <div className="bg-silver flex flex-col px-3 w-[25%] ">
             <SmallPresentationContainer/>
             <Skills/>
         </div>
